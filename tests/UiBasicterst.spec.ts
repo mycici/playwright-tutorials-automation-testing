e80efded25ca13e729146c049958d8d64866ab6e
ahotyanovich <hotyanovicha@gmail.com>
2025-06-26 19:05:03 +0200

    section 3 done

---
+++ tests/UiBasicterst.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from "@playwright/test";
+import { assert } from "console";
+
+const {test} = require ('@playwright/test');
+
+
+
+test('First test', async ({browser, page})=>
+{
+    // const context = await browser.newContext()
+    // const page = await context.newPage();
+    
+    await page.goto('https://google.com')
+    await expect(page).toHaveTitle("Google")
+
+
+})
