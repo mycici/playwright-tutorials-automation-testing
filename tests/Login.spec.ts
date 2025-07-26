a3fcd1a1cb969d1ae9fbf8604a8c28d7673edc8e
ahotyanovich <hotyanovicha@gmail.com>
2025-07-26 09:24:19 +0200

    login

---
+++ tests/Login.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test('Login with valid regular user', async ({page}) => {
+    await page.goto('https://rahulshettyacademy.com/client/#/auth/login')
+
+    //работают:
+    //await page.getByPlaceholder('email@example.com').fill('anshika@gmail.com')
+    //await page.getByPlaceholder('enter your passsword').fill('Iamking@000')
+    await page.locator('#userEmail').fill('tonuza7211@dropjar.com')
+    await page.locator('#userPassword').fill("123qweASD")
+
+    //не работают:
+    //await page.getByRole('textbox', {name:"userEmail"}).fill('anshika@gmail.com')
+    //await page.getByRole('textbox', {name:"Password"}).fill('Iamking@000')
+
+    await page.getByRole('button', {name: 'Login'}).click()
+    await expect(page.getByRole('button', {name: 'Sign Out'})).toBeVisible()
+
+})
--- tests/UiBasicterst.spec.ts
+++ tests/UiBasicterst.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "@playwright/test";
-import { assert } from "console";
+
 
 const {test} = require ('@playwright/test');
 
