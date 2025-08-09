3a856139bb533cf619c24a95be68e175c98361c4
ahotyanovich <hotyanovicha@gmail.com>
2025-08-09 09:26:29 +0200

    Fixed Login page fixture and test

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,15 +1,25 @@
 import { BasePage } from "./BasePage";
-import { Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 import { Urls } from "../test-data/common/page-url-endpoints";
 
 export class LoginPage extends BasePage{
+    emailInput: Locator;
+    passwordInput: Locator;
+    loginButton: Locator;
+
     constructor(page: Page){
         super(page);
+        this.emailInput = this.page.locator('#userEmail');
+        this.passwordInput = this.page.locator('#userPassword');
+        this.loginButton = this.page.locator('#login');
     }
 
     async login(email: string, password: string){
-        await this.page.locator('#userEmail').fill(email);
-        await this.page.locator('#userPassword').fill(password);
-        await this.page.getByRole('button', {name: 'Login'}).click();
+        await this.emailInput.fill(email);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+    async verifyLogin(){
+        await expect(this.page.getByRole('button', {name: 'Sign Out'})).toBeVisible()
     }
 }
