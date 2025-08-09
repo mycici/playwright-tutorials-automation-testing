2bb23191d0f2229a1ef1da633a10757e847bf714
mycici <40543995+mycici@users.noreply.github.com>
2025-08-09 09:27:04 +0200

    Fixed Login page fixture and test (#3)
    Co-authored-by: ahotyanovich <hotyanovicha@gmail.com>

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,13 +1,14 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import { Urls } from "../test-data/common/page-url-endpoints";
 import { DefaultUser } from '../test-data/users';
 
-test('Login with valid regular user', async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.visit(Urls.login);
-    await loginPage.login(DefaultUser.email, DefaultUser.password);
-    await expect(page.getByRole('button', {name: 'Sign Out'})).toBeVisible()
-
-})
+test('Login with valid regular user',
+    { tag: '@P1' },
+    async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.visit(Urls.login);
+        await loginPage.login(DefaultUser.email, DefaultUser.password);
+        await loginPage.verifyLogin();
+    })
 
