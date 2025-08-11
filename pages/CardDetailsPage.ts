a5b0e5e134f609b23456c17ad3fbb228110494d4
ahotyanovich <hotyanovicha@gmail.com>
2025-08-11 15:09:04 +0200

    added cardDetailsPage

---
+++ pages/CardDetailsPage.ts
@@ -0,0 +1,30 @@
+import { Locator, Page, expect } from "@playwright/test";
+import { BasePage } from "./BasePage";
+
+class CardDetailsPage extends BasePage {
+    title: Locator;
+    price: Locator;
+    section: Locator;
+
+    constructor(page: Page) {
+        super(page);
+        this.section = this.page.locator('.container');
+        this.title = this.section.getByRole('heading', { level: 2 });
+        this.price = this.section.getByRole('heading', { level: 3 });
+    }
+
+    async getCardDetails() {
+        const [title, price] = await Promise.all([
+            this.title.innerText(),
+            this.price.innerText()
+        ]);
+        return { title, price };
+    }
+
+    async verifyCardDetails(expectedCardDetils: {title: string, price: string}){
+        await expect.soft(this.title).toHaveText(expectedCardDetils.title);
+        await expect.soft(this.price).toHaveText(expectedCardDetils.price);
+
+    }
+}
+export default CardDetailsPage;
--- tests/UiBasicterst.spec.ts
+++ tests/UiBasicterst.spec.ts
@@ -1,15 +1,12 @@
 import { test, expect } from "../fixtures/fixtures";
 import HomePage from "../pages/HomePage";
+import CardDetailsPage from "../pages/CardDetailsPage";
 
 test('Random card details match between list and detail pages', async ({page}) => {
     const homePage = new HomePage(page);
     await homePage.openHomePage();
     
     const cardDetails = await homePage.openRandomCardAndGetDetails();
-    
-    await expect(page.locator('.container').getByRole('heading', {level: 2}))
-        .toHaveText(cardDetails.title);
-    await expect(page.locator('.container').getByRole('heading', {level: 3}))
-        .toHaveText(cardDetails.price);
-    await expect(page.getByRole('button', {name: 'Add to Cart'})).toBeVisible();
+    const cardDetailsPage = new CardDetailsPage(page);
+    await cardDetailsPage.verifyCardDetails(cardDetails);
 });
